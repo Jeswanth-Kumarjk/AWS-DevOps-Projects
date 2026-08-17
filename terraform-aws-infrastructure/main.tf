32fcc9dfa5ca6edbb4e0469b763350d2f91a5959
Jeswanth Kumar <jeswanthjk19@gmail.com>
2026-08-17 23:24:19 +0530

    Add S3 versioning with Terraform

--- terraform-aws-infrastructure/main.tf
+++ terraform-aws-infrastructure/main.tf
@@ -1,15 +1,22 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
+      source = "hashicorp/aws"
     }
   }
 }
 
 provider "aws" {
-  region = "ap-south-1"
+  region = var.aws_region
 }
 
 resource "aws_s3_bucket" "terraform_bucket" {
-  bucket = "jeswanth-terraform-demo-bucket-2026"
+  bucket = var.bucket_name
+}
+resource "aws_s3_bucket_versioning" "terraform_bucket_versioning" {
+  bucket = aws_s3_bucket.terraform_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
